5c2b9f3afe7d3adaf5a06e3a7f086ddaa594c530
Tristan Steffen <tristansteffen@gmx.de>
2024-10-04 15:33:43 +0200

    added search functionallity

--- app/exercises/exerciseSelector.tsx
+++ app/exercises/exerciseSelector.tsx
@@ -12,6 +12,7 @@ const ExerciseSelector: React.FC = () => {
   const db = useSQLiteContext();
   const { colors } = useTheme() as CustomTheme;
   const [searchValue, setSearchValue] = useState<string>("");
+  const [allExercises, setAllExercises] = useState<Exercise[] | null>(null);
   const [groupedExercises, setGroupedExercises] = useState<Exercise[][] | null>(
     null
   );
@@ -20,26 +21,28 @@ const ExerciseSelector: React.FC = () => {
   useEffect(() => {
     const fetchExercises = async () => {
       const response = await getAllExercises(db);
-      const grouped = response.reduce((acc: Exercise[][], exercise) => {
-        const primaryMuscle =
-          exercise.targetMuscles?.primary?.[0]?.name || "Unknown";
-        const muscleGroupIndex = acc.findIndex(
-          (group) =>
-            group[0].targetMuscles?.primary?.[0]?.name === primaryMuscle
-        );
-        if (muscleGroupIndex > -1) {
-          acc[muscleGroupIndex].push(exercise);
-        } else {
-          acc.push([exercise]);
-        }
-        return acc;
-      }, []);
-
-      setGroupedExercises(grouped);
+      setAllExercises(response);
+      setGroupedExercises(groupExercises(response));
     };
 
     fetchExercises();
   }, [db]);
+
+  const groupExercises = (exercises: Exercise[]) => {
+    return exercises.reduce((acc: Exercise[][], exercise) => {
+      const primaryMuscle =
+        exercise.targetMuscles?.primary?.[0]?.name || "Unknown";
+      const muscleGroupIndex = acc.findIndex(
+        (group) => group[0].targetMuscles?.primary?.[0]?.name === primaryMuscle
+      );
+      if (muscleGroupIndex > -1) {
+        acc[muscleGroupIndex].push(exercise);
+      } else {
+        acc.push([exercise]);
+      }
+      return acc;
+    }, []);
+  };
 
   const handleExerciseSelect = (exercise: Exercise, selected: boolean) => {
     if (selected) {
@@ -51,13 +54,26 @@ const ExerciseSelector: React.FC = () => {
     }
   };
 
+  const onSearch = (value: string) => {
+    setSearchValue(value);
+
+    if (!value) {
+      setGroupedExercises(groupExercises(allExercises!));
+    } else {
+      const filteredExercises = allExercises?.filter((exercise) =>
+        exercise.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setGroupedExercises(groupExercises(filteredExercises!));
+    }
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <SearchInput
           placeholder="Search"
           value={searchValue}
-          onChangeText={setSearchValue}
+          onChangeText={onSearch}
           style={{ marginBottom: 20 }}
         />
         {groupedExercises &&
